ea988e6ddb3801eb5e7b0eabb6603ea61d453f1f
gilyounggood <https://github.com/gilyounggood/gamemarket.git>
2022-05-29 15:52:53 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,23 @@ function login() {
         id: id.value,
         psword: psword.value, 
     };
-    console.log(req);
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("로그인 중 에러 발생");
+        });
 }
